aee9b6c3fefa34add3b8b02747484f0795d4cf42
Michael Caterisano <mcaterisano@gmail.com>
2021-02-25 15:38:06 -0500

    add logout button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
 import logo from "./logo.svg";
+import {useAuth0} from "@auth0/auth0-react";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import LoginButton from "./LoginButton";
+import LogoutButton from "./LogoutButton";
 import "./App.css";
 
-function App() {
+export default function App() {
+  const {user, isAuthenticated, isLoading} = useAuth0();
+
   return (
-    <div className="App">
-      <LoginButton />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          {!isAuthenticated && <LoginButton />}
+          {isAuthenticated && <LogoutButton />}
+
+          {isAuthenticated && (
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          )}
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {Auth0Provider} from '@auth0/auth0-react';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import {Auth0Provider} from "@auth0/auth0-react";
 
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <Auth0Provider
     domain="seoulspice.us.auth0.com"
     clientId="6GnBBRY6dxTpjTbxFNvqdhRCYEm09Vvl"
-    redirectUri={window.location.origin}
+    redirectUri={`${window.location.origin}/about`}
   >
     <App />
   </Auth0Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
